480f6a4fea2a03ebcec87ada04ad21e3b3376728
Boboyev Dilshod <pers9712@gmail.com>
2021-06-10 14:28:04 +0600

    practice java

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static Movie[] getMovie() {
+        return new Movie[]{
+                new Movie("Начало", 2010, 11),
+                new Movie("Веном 2", 2021, 5),
+        };
+    }
+
+    public static Theatre[] getTheatre() {
+        return new Theatre[]{
+                new Theatre("Анна Каренина", 2017, 16),
+                new Theatre("Шелкунчик", 2010, 11),
+        };
+    }
+
+    public static void validEvent(Event event) {
+        if (event.age == 0 || event.releaseYear == 0 || event.title == null) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (Event event : getMovie()) {
+            validEvent(event);
+        }
+        for (Event event : getTheatre()) {
+            validEvent(event);
+        }
+        System.out.println("Все события корректны");
+    }
+}
